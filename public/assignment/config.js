f0a82b298d4f84b462b9d526394cd4a245aac497
chandrika2311 <chandrika2311@gmail.com>
2017-03-01 23:36:37 -0500

    Forth

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -66,17 +66,17 @@
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget", {
-                templateUrl: "views/widget/template/widget-list.view.client.html",
+                templateUrl: "views/widget/Template/widget-list.view.client.html",
                 controller:"WidgetListController",
                 controllerAs: "model"
                 })
             .when("/user/:uid/website/:wid/page/:pid/widget/new", {
-                templateUrl: "views/widget/template/widget-chooser.view.client.html",
+                templateUrl: "views/widget/Template/widget-chooser.view.client.html",
                 controller: "NewWidgetController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/:wgid",{
-                templateUrl: "views/widget/template/widget-edit.view.client.html",
+                templateUrl: "views/widget/Template/widget-edit.view.client.html",
                 controller: "WidgetEditController",
                 controllerAs: "model"
             })
